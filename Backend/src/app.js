4095e03b96d88b27e48ffca77df1406f2dee285e
garvitkumar854 <garvitkajot854@gmail.com>
2026-03-29 16:44:38 +0530

    Updated

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 
 const authRoutes = require('./routes/auth.routes');
+const config = require('./config/config');
 
 const cookieParser = require('cookie-parser');
 
 const app = express();
 
 // Middlewares
+app.use(cors({
+	origin: config.CLIENT_URL,
+	credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 
--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -6,6 +6,7 @@ const config = {
 	JWT_SECRET: process.env.JWT_SECRET,
 	NODE_ENV: process.env.NODE_ENV || 'development',
 	port: Number.parseInt(process.env.PORT || '5000', 10),
+	CLIENT_URL: process.env.CLIENT_URL || 'https://nova-auth-lyart.vercel.app',
 	mongoUri: process.env.MONGO_URI,
 	GOOGLE_USER: process.env.GOOGLE_USER,
 	GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
